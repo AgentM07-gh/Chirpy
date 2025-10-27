b82e0352d840e1ab47cd1e15dcca0c76326244ff
AgentM07-gh <michael.gent@gmail.com>
2025-10-27 17:43:51 -0400

    Ch1L1

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	ServeMux := http.NewServeMux()
+
+	server := &http.Server{
+		Handler: ServeMux,
+		Addr:    ":8080",
+	}
+
+	log.Println("Server is listening on port 8080...")
+	// Start the server.
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
